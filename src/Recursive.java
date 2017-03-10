689bec9f1bb3820b969d025411e2954fde394fad
Nishant Rathore <nishant7rathore@gmail.com>
2017-03-10 19:35:35 -0330

    Towers of Hanoi Simple program

--- src/Recursive.java
+++ src/Recursive.java
@@ -4,16 +4,34 @@
 
 /**
  * @author nishant7rathore
- *
+ * Thanks to http://www.javawithus.com/programs/towers-of-hanoi
  */
-public class Recursive {
+public class Recursive 
+{
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
- 
+	public static void main(String[] args) 
+	{
+		TowerOfHanoi(3,"A","B","C");
+	
 	}
 
+	public static void TowerOfHanoi(int n, String start, String auxillary, String end)
+	{
+		if(n == 1)
+		{
+			//Just put disk from start to end
+			System.out.println(start + " =>>>>>" + end);
+		}
+		else
+		{
+			TowerOfHanoi(n-1,start,end,auxillary);
+			System.out.println(start + " =>>>>>" + end);
+			TowerOfHanoi(n-1,auxillary,start,end);
+		}
+	}
+	
+	
 }
